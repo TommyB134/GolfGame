6633c3e6c62932d2726c224d104588d03fbe037c
Thomas Bowler <Thomaslbowler@gmail.com>
2018-05-03 17:48:50 +1000

    New MP Initialisation
    Attempting a new way at MP using: https://youtu.be/_XyjMeAb3fw?list=PLZlv_N0_O1gYqSlbGQVKsRg6fpxWndZqZ

---
+++ MiniGolfRush/Intermediate/Build/Win64/MiniGolfRush/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
@@ -0,0 +1,1 @@
+#include "E:/GitHub/MiniGolfRush/MiniGolfRush/Intermediate/Build/Win64/MiniGolfRush/Development/UELinkerFixups/PCH.UELinkerFixups.h"
---
+++ MiniGolfRush/Intermediate/Build/Win64/MiniGolfRush/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for E:\GitHub\MiniGolfRush\MiniGolfRush\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
+#include "E:\GitHub\MiniGolfRush\MiniGolfRush\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
